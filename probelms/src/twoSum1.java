df31e9e0468a58a39c1f7272946ba29094ecbd8e
Ahmedsherif74 <ahmedsherifsaid2000@gmail.com>
2025-05-16 22:46:25 +0300

    twoSum

---
+++ probelms/src/twoSum1.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class twoSum1 {
+    public int[] twosum(int[] nums, int target){
+        HashMap <Integer , Integer> values = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            values.put(nums[i] ,i);
+        }
+        for (int i = 0; i <nums.length; i++) {
+            int diff = target - nums[i];
+            if (values.containsKey(diff) && values.get(diff)!=i) {
+                return new int[]{i, values.get(diff)};
+
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        twoSum1 checher = new twoSum1();
+
+        int[] result =checher.twosum(new int []{1,2,5} , 7);
+        System.out.println("Result is " + Arrays.toString(result));
+
+
+
+
+    }
+}
