3b54be4ab3e4381c9ad25137b51d224016450882
Ahmedsherif74 <ahmedsherifsaid2000@gmail.com>
2025-08-17 22:46:47 +0300

    TwoSum

--- probelms/src/twoSum1.java
+++ probelms/src/twoSum1.java
@@ -4,16 +4,19 @@ import java.util.HashMap;
 public class twoSum1 {
     public int[] twosum(int[] nums, int target){
         HashMap <Integer , Integer> values = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            values.put(nums[i] ,i);
+        for (int i =0 ; i < nums.length; i++ ){
+            values.put(nums[i] , i);
         }
-        for (int i = 0; i <nums.length; i++) {
-            int diff = target - nums[i];
-            if (values.containsKey(diff) && values.get(diff)!=i) {
-                return new int[]{i, values.get(diff)};
+        for (int i=0 ; i<nums.length; i++){
+            int diff = target-nums[i];
+            if(values.containsKey(diff)&&values.get(diff)!=i){
+                return new int[] {i,values.get(diff)};
             }
+
         }
-        return new int[0];
+
+        return  new int[0];
+
     }
 
     public static void main(String[] args) {
